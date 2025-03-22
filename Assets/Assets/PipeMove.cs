7d70488bfd4cd10c440c69a447e66a9805bbce04
anwrat <anweshrwt22@gmail.com>
2025-03-22 14:24:44 +0545

    Update PipeMove.cs
    Destroying the pipes if they go across a certain x value in left side of screen

--- Assets/Assets/PipeMove.cs
+++ Assets/Assets/PipeMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PipeMove : MonoBehaviour
 {
     public float movespeed = 5;
+    public float deadzone = -41;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,9 @@ public class PipeMove : MonoBehaviour
     void Update()
     {
         transform.position = transform.position+(Vector3.left*movespeed) * Time.deltaTime; //Time.delataTime makes sure the movespeed isnot affected by framerate
+        if (transform.position.x < deadzone)
+        {
+            Destroy(gameObject);
+        }
     }
 }
