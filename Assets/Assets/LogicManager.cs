d38754ca0e626cf1c2325977432af36edf12052b
anwrat <anweshrwt22@gmail.com>
2025-03-22 17:45:54 +0545

    Adding a play again logic
    Added a restart game function to logicmanager script and added that function to onclick component of button

--- Assets/Assets/LogicManager.cs
+++ Assets/Assets/LogicManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicManager : MonoBehaviour
 {
@@ -13,5 +14,10 @@ public class LogicManager : MonoBehaviour
     {
         playerScore += score;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
