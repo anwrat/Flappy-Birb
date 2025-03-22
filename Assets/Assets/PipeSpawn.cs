61e9778c9f5021d470cd9b9a48fff1eab7c0525c
anwrat <anweshrwt22@gmail.com>
2025-03-22 14:07:54 +0545

    Updating PipeSpawn script
    Added a spawnrate and timer so that the pipe spawns at intervals

--- Assets/Assets/PipeSpawn.cs
+++ Assets/Assets/PipeSpawn.cs
@@ -7,6 +7,9 @@ public class PipeSpawn : MonoBehaviour
     //Reference to the pipe object
     public GameObject pipe;
 
+    public float spawnrate = 2;
+    private float timer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,15 @@ public class PipeSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Spawn the pipe using instantiate
-        Instantiate(pipe,transform.position,transform.rotation);
+        if (timer < spawnrate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            //Spawn the pipe using instantiate
+            Instantiate(pipe,transform.position,transform.rotation);
+            timer = 0;
+        }
     }
 }
