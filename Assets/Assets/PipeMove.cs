ee32b3b4fd454efd112b593cf1d2b640b854086b
anwrat <anweshrwt22@gmail.com>
2025-03-22 13:51:19 +0545

    Creating a pipe object
    Create a parent pipe object that has two childs top and bottom pipe with boxcolliders. The parent object has a script that transforms the position to left

---
+++ Assets/Assets/PipeMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMove : MonoBehaviour
+{
+    public float movespeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position+(Vector3.left*movespeed) * Time.deltaTime; //Time.delataTime makes sure the movespeed isnot affected by framerate
+    }
+}
